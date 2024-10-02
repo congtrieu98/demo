719b8dae057d986e66f3b47cf17604fe73135d7d
trieu_ilotusland <trieu.nguyen@ilotusland.com>
2024-10-02 17:25:54 +0700

    update redirect_uri

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -6,7 +6,7 @@ export const LoginForm = () => {
   const router = useRouter();
   const client_id = 'babc5bac-d836-4afc-afe7-f3374fb9fefd';
   // const client_secret = '286a01083ee5c6e1260179999ff97ced';
-  const redirect_uri = 'http://localhst:3000';
+  const redirect_uri = 'http://localhost:3000';
   const code_challenge = process.env.NEXT_PUBLIC_CODE_CHALLENGE;
   const url = `http://localhost:3001/login?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&code_challenge=${code_challenge}&code_challenge_method=S256`;
   return (
